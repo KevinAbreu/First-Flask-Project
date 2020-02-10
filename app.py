f9e44426ffb32554ddae6ecc40b78a9be23ca189
Kevin Abreu <kevinrabreu@hotmail.com>
2020-02-09 21:25:49 -0500

    Second commit of the day

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    completed = db.Column(db.Integer, default = 0)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+# 'from app import db' on bash
+# Then db.create_all to create datebase
 
 @app.route('/')
 def index():
@@ -8,8 +25,8 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
+# 19:53
+# env\Scripts\activate.bat
 
 
 # print("Hello! :)")
